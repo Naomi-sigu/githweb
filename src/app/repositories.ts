385712489338b55fa0c3ce241115754cd2e0221c
Naomi <siguneema@gmail.com>
2019-08-30 22:12:39 +0300

    add changes to repo

--- src/app/repositories.ts
+++ src/app/repositories.ts
@@ -1,8 +1,8 @@
 export class Repositories {
     constructor(
+      public html_url : string,
       public name:string,
-      public description:string,
-      public html_url : string
+      public description:string
       )
       {}
   }
